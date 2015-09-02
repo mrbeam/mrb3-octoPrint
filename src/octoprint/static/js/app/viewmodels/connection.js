1c018ec277f42c98f2b2775646c170c6011d1652
Teja <mail@tejaphilipp.de>
2015-09-02 12:04:21 +0200

    added visual flashing bar.

--- src/octoprint/static/js/app/viewmodels/connection.js
+++ src/octoprint/static/js/app/viewmodels/connection.js
@@ -19,120 +19,6 @@ $(function() {
         self.printerProfiles.currentProfile.subscribe(function() {
             self.selectedPrinter(self.printerProfiles.currentProfile());
         });
-//<<<<<<< HEAD
-//        self.printerOptions(printerOptions);
-//    });
-//
-//    self.printerProfiles.currentProfile.subscribe(function() {
-//        self.selectedPrinter(self.printerProfiles.currentProfile());
-//    });
-//
-//    self.portOptions = ko.observableArray(undefined);
-//    self.baudrateOptions = ko.observableArray(undefined);
-//    self.printerOptions = ko.observableArray(undefined);
-//    self.selectedPort = ko.observable(undefined);
-//    self.selectedBaudrate = ko.observable(undefined);
-//    self.selectedPrinter = ko.observable(undefined);
-//    self.saveSettings = ko.observable(undefined);
-//    self.autoconnect = ko.observable(undefined);
-//
-//    self.isErrorOrClosed = ko.observable(undefined);
-//    self.isConnecting = ko.observable(undefined);
-//    self.isOperational = ko.observable(undefined);
-//    self.isLocked = ko.observable(undefined);
-//    self.isPrinting = ko.observable(undefined);
-//    self.isPaused = ko.observable(undefined);
-//    self.isError = ko.observable(undefined);
-//    self.isReady = ko.observable(undefined);
-//    self.isLoading = ko.observable(undefined);
-//
-//    self.buttonText = ko.computed(function() {
-//        if (self.isErrorOrClosed())
-//            return gettext("Connect");
-//        else
-//            return gettext("Disconnect");
-//    });
-//
-//    self.previousIsOperational = undefined;
-//
-//    self.requestData = function() {
-//        $.ajax({
-//            url: API_BASEURL + "connection",
-//            method: "GET",
-//            dataType: "json",
-//            success: function(response) {
-//                self.fromResponse(response);
-//            }
-//        })
-//    };
-//
-//    self.fromResponse = function(response) {
-//        var ports = response.options.ports;
-//        var baudrates = response.options.baudrates;
-//        var portPreference = response.options.portPreference;
-//        var baudratePreference = response.options.baudratePreference;
-//        var printerPreference = response.options.printerProfilePreference;
-//        var printerProfiles = response.options.printerProfiles;
-//
-//        self.portOptions(ports);
-//        self.baudrateOptions(baudrates);
-//
-//        if (!self.selectedPort() && ports && ports.indexOf(portPreference) >= 0)
-//            self.selectedPort(portPreference);
-//        if (!self.selectedBaudrate() && baudrates && baudrates.indexOf(baudratePreference) >= 0)
-//            self.selectedBaudrate(baudratePreference);
-//        if (!self.selectedPrinter() && printerProfiles && printerProfiles.indexOf(printerPreference) >= 0)
-//            self.selectedPrinter(printerPreference);
-//
-//        self.saveSettings(false);
-//    };
-//
-//    self.fromHistoryData = function(data) {
-//        self._processStateData(data.state);
-//    };
-//
-//    self.fromCurrentData = function(data) {
-//        self._processStateData(data.state);
-//    };
-//
-//    self._processStateData = function(data) {
-//        self.previousIsOperational = self.isOperational();
-//        self.isErrorOrClosed(data.flags.closedOrError);
-//        self.isOperational(data.flags.operational);
-//        self.isLocked(data.flags.locked);
-//        self.isConnecting(data.text === "Connecting" || data.text === "Opening serial port");
-//        self.isPaused(data.flags.paused);
-//        self.isPrinting(data.flags.printing);
-//        self.isError(data.flags.error);
-//        self.isReady(data.flags.ready);
-//        self.isLoading(data.flags.loading);
-//
-//        var connectionTab = $("#connection");
-//        if (self.previousIsOperational != self.isOperational()) {
-//            if (self.isOperational() && connectionTab.hasClass("in")) {
-//                // connection just got established, close connection tab for now
-////                connectionTab.collapse("hide");
-//            } else if (!connectionTab.hasClass("in")) {
-//                // connection just dropped, make sure connection tab is open
-////                connectionTab.collapse("show");
-//            }
-//        }
-//    };
-//
-//    self.connect = function() {
-//        if (self.isErrorOrClosed()) {
-//            var data = {
-//                "command": "connect",
-//                "port": self.selectedPort(),
-//                "baudrate": self.selectedBaudrate(),
-//                "printerProfile": self.selectedPrinter(),
-//                "autoconnect": self.settings.serial_autoconnect()
-//            };
-//
-//            if (self.saveSettings())
-//                data["save"] = true;
-//=======
-//>>>>>>> upstream/maintenance
 
         self.portOptions = ko.observableArray(undefined);
         self.baudrateOptions = ko.observableArray(undefined);
@@ -150,6 +36,7 @@ $(function() {
         self.isError = ko.observable(undefined);
         self.isReady = ko.observable(undefined);
         self.isLoading = ko.observable(undefined);
+        self.isFlashing = ko.observable(undefined);
 
         self.buttonText = ko.computed(function() {
             if (self.isErrorOrClosed())
@@ -210,6 +97,7 @@ $(function() {
             self.isError(data.flags.error);
             self.isReady(data.flags.ready);
             self.isLoading(data.flags.loading);
+            self.isFlashing(data.flags.flashing);
 
             var connectionTab = $("#connection_wrapper");
             if (self.previousIsOperational != self.isOperational()) {
