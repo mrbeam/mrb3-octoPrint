2b3ebaf14d49da4f41c3c67e1d38b64a6dd1f2da
make-ing <bckr.florian@gmail.com>
2015-09-23 14:37:34 +0200

    fixed null bug

--- src/octoprint/static/js/app/viewmodels/printerstate.js
+++ src/octoprint/static/js/app/viewmodels/printerstate.js
@@ -123,7 +123,14 @@ $(function() {
             self._processProgressData(data.progress);
             self._processZData(data.currentZ);
             self._processBusyFiles(data.busyFiles);
-            self.currentPos({x: data.workPosition[0], y: data.workPosition[1]});
+            self._processWPosData(data.workPosition);
+        };
+        self._processWPosData = function(data) {
+            if (data == null) {
+                self.currentPos({x: 0, y: 0});
+            } else {
+                self.currentPos({x: data.workPosition[0], y: data.workPosition[1]});
+            }
         };
         self._processStateData = function(data) {
             var prevPaused = self.isPaused();
