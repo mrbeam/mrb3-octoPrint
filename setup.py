e86678a1ab74442e1f1eac95cf33dd2216935342
amrithvenkat <amrithvenkat1212@gmail.com>
2025-11-19 11:14:24 +0100

    update versioneer for py3.12

--- setup.py
+++ setup.py
@@ -274,6 +274,7 @@ def params():
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: JavaScript",
         "Topic :: Printing",
