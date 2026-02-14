34e4a0c9d010aaa6a7123b0dbf667d1b5ca9aa68
Teja <teja@mr-beam.org>
2026-02-14 23:43:54 +0100

    fixes ... hopefully

--- tests/cypress/cypress.config.js
+++ tests/cypress/cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
     baseUrl: "http://127.0.0.1:5000",
     specPattern: "integration/**/*.js", // Replaces integrationFolder
     supportFile: "support/index.js",
+    retries: {
+      runMode: 2,   // Number of retries when running 'cypress run'
+      openMode: 0,  // Number of retries when running 'cypress open'
+    },
     setupNodeEvents(on, config) {
       // Replaces pluginsFile
       return require("./plugins/index.js")(on, config);
