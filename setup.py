7a762d3aa9165ba16ee9ae87bb690be35706db2c
Teja <teja@mr-beam.org>
2026-02-15 20:56:46 +0100

    re-added packaging dependency - was formerly in pgk_resource

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ SETUP_REQUIRES = ["markdown>=3.7,<4"]
 # Modernized requirements for Python 3.10+
 # Pinned to versions compatible with Python 3.13 (removes 'imp' and 'pathtools' dependencies)
 INSTALL_REQUIRES = [
+    "packaging>=23.2",
     "setuptools",
     "OctoPrint-FileCheck>=2021.2.23",
     "OctoPrint-FirmwareCheck>=2021.10.11",
