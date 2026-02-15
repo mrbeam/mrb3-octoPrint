49a612773c193a08b2b2858d7257b3e3ef0881db
Teja <teja@mr-beam.org>
2026-02-15 18:23:02 +0100

    again

--- src/octoprint/plugin/types.py
+++ src/octoprint/plugin/types.py
@@ -1413,7 +1413,7 @@ class BlueprintPlugin(OctoPrintPlugin, RestartNeedingPlugin):
 
         kwargs = self.get_blueprint_kwargs()
         blueprint = flask.Blueprint(
-            "plugin_" + self._identifier.replace(".", "_"), self._identifier, **kwargs
+            "plugin_" + self._identifier.replace(".", "_") + "_logic", self._identifier, **kwargs
         )
 
         # we now iterate over all members of ourselves and look if we find an attribute
--- src/octoprint/server/__init__.py
+++ src/octoprint/server/__init__.py
@@ -1842,19 +1842,25 @@ class Server(object):
         return blueprint, url_prefix
 
     def _prepare_asset_plugin(self, plugin):
-        # name = plugin._identifier
-        name = plugin._identifier.replace(".", "_")
+        # Use the identifier as the folder name in the URL, but the safe name for the blueprint
+        safe_name = plugin._identifier.replace(".", "_")
 
-        url_prefix = "/plugin/{name}".format(name=name)
+        url_prefix = "/plugin/{name}".format(name=plugin._identifier)
+
         blueprint = Blueprint(
-            "plugin_" + name + "_assets", name, static_folder=plugin.get_asset_folder()
+            "plugin_" + safe_name + "_assets",
+            plugin.__module__, # Use the actual module path here
+            static_folder=plugin.get_asset_folder(),
+            static_url_path="/static" # Relative to the blueprint's url_prefix
         )
+
+        # We use 'app' from the outer scope, which should be the Flask instance
         app.register_blueprint(blueprint, url_prefix=url_prefix)
 
         if self._logger:
             self._logger.debug(
                 "Registered assets of plugin {name} under URL prefix {url_prefix}".format(
-                    name=name, url_prefix=url_prefix
+                    name=plugin._identifier, url_prefix=url_prefix
                 )
             )
 
