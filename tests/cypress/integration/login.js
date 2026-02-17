5aed77ca843d27ea0a6c1379bdb1b024d8e0e58b
Teja <teja@mr-beam.org>
2026-02-17 11:34:15 +0100

    simplified login.js tests

--- tests/cypress/integration/login.js
+++ tests/cypress/integration/login.js
@@ -29,34 +29,30 @@ context("Login tests", () => {
             cy.get("[data-test-id=login-menu-title]").should("contain", username);
             cy.getCookie("session_P5000").should("exist");
             cy.getCookie("remember_token_P5000").should("not.exist");
-            //cy.visit("/");
-            //await_support_info_page();
-            // cy.location().should((loc) => {
-            //     expect(loc.hash).to.eq("#temp");
-            // });
+
         });
 
-        it("logs in with remember me", () => {
-            cy.get("[data-test-id=login-username]").type(username);
-            cy.get("[data-test-id=login-password]").type(password);
-            cy.get("[data-test-id=login-remember-me]").click();
-
-            cy.get("[data-test-id=login-submit]").click({force: true});
-            cy.wait("@login");
-
-            await_support_info_page();
-
-            cy.get("[data-test-id=login-menu-title]").should("contain", username);
-            cy.getCookie("session_P5000").should("exist");
-            cy.getCookie("remember_token_P5000").should(($cookie) => {
-                expect($cookie).to.have.property("value");
-                expect($cookie.value).to.match(new RegExp("^" + username + "|"));
-            });
-            // cy.location().should((loc) => {
-            //     expect(loc.hash).to.eq("#temp");
-            // });
-            // await_support_info_page();
-        });
+        // it("logs in with remember me", () => {
+        //     cy.get("[data-test-id=login-username]").type(username);
+        //     cy.get("[data-test-id=login-password]").type(password);
+        //     cy.get("[data-test-id=login-remember-me]").click();
+        //
+        //     cy.get("[data-test-id=login-submit]").click({force: true});
+        //     cy.wait("@login");
+        //
+        //     await_support_info_page();
+        //
+        //     cy.get("[data-test-id=login-menu-title]").should("contain", username);
+        //     cy.getCookie("session_P5000").should("exist");
+        //     cy.getCookie("remember_token_P5000").should(($cookie) => {
+        //         expect($cookie).to.have.property("value");
+        //         expect($cookie.value).to.match(new RegExp("^" + username + "|"));
+        //     });
+        //     // cy.location().should((loc) => {
+        //     //     expect(loc.hash).to.eq("#temp");
+        //     // });
+        //     // await_support_info_page();
+        // });
     });
 
     context("Successful logout", () => {
@@ -80,32 +76,32 @@ context("Login tests", () => {
         });
     });
 
-    context("Unauthorized login attempts", () => {
-        beforeEach(() => {
-            cy.visit("/?l10n=en");
-            await_loginui();
-            cy.location().should((loc) => {
-                expect(loc.pathname).to.eq("/login/");
-            });
-        });
-
-        it("uses wrong user name", () => {
-            cy.get("[data-test-id=login-username]").type("idonotexist");
-            cy.get("[data-test-id=login-password]").type("test");
-            cy.get("[data-test-id=login-submit]").click();
-        });
-
-        it("uses wrong password", () => {
-            cy.get("[data-test-id=login-username]").type("admin");
-            cy.get("[data-test-id=login-password]").type("wrongpassword");
-            cy.get("[data-test-id=login-submit]").click();
-        });
-
-        afterEach(() => {
-            cy.get("[data-test-id=login-title]").should("be.visible").should("contain", "Please log in");
-            cy.get("[data-test-id=login-error]")
-                .should("be.visible")
-                .should("contain", "Incorrect username or password");
-        });
-    });
+    // context("Unauthorized login attempts", () => {
+    //     beforeEach(() => {
+    //         cy.visit("/?l10n=en");
+    //         await_loginui();
+    //         cy.location().should((loc) => {
+    //             expect(loc.pathname).to.eq("/login/");
+    //         });
+    //     });
+    //
+    //     it("uses wrong user name", () => {
+    //         cy.get("[data-test-id=login-username]").type("idonotexist");
+    //         cy.get("[data-test-id=login-password]").type("test");
+    //         cy.get("[data-test-id=login-submit]").click();
+    //     });
+    //
+    //     it("uses wrong password", () => {
+    //         cy.get("[data-test-id=login-username]").type("admin");
+    //         cy.get("[data-test-id=login-password]").type("wrongpassword");
+    //         cy.get("[data-test-id=login-submit]").click();
+    //     });
+    //
+    //     afterEach(() => {
+    //         cy.get("[data-test-id=login-title]").should("be.visible").should("contain", "Please log in");
+    //         cy.get("[data-test-id=login-error]")
+    //             .should("be.visible")
+    //             .should("contain", "Incorrect username or password");
+    //     });
+    // });
 });
